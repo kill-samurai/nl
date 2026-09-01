60f68a8b0652fd865e0fb3c8a91b413fc5b9c796
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2026-09-01 13:16:04 -0400

    Update personal.sh to install additional applications
    Uncommented installation commands for various applications and added WezTerm to dock configuration.

--- personal.sh
+++ personal.sh
@@ -12,31 +12,20 @@ cp ./ohmyzsh-theme-passion/passion.zsh-theme ~/.oh-my-zsh/themes/passion.zsh-the
 brew install coreutils && 
 curl -O https://raw.githubusercontent.com/kill-samurai/nl/refs/heads/main/.zshrc &&
 softwareupdate --install-rosetta --agree-to-license &&
-#brew install --cask postman && 
-#brew install --cask google-chrome && 
 brew install node && 
-brew install --cask visual-studio-code && 
-#brew install --cask dadroit-json-viewer && 
+brew install --cask visual-studio-code &&  
 brew install python@3.13 && 
-#brew install --cask pycharm-ce && 
-#brew install --cask firefox && 
 brew install --cask sublime-text && 
 brew install --cask spotify && 
 brew install --cask signal && 
-#brew install --cask studio-3t && 
 brew install htop && 
-#brew install --cask 1password &&
-#brew install --cask slack &&
-#brew install defaultbrowser &&
 brew install --cask steam &&
 brew install dockutil &&
 brew install --cask virtualbuddy &&
 brew install --cask brave-browser &&
 brew install --cask whatsapp &&
-#brew install --cask telegram &&
+brew install --cask wezterm &&
 #config below
-#defaultbrowser brave &&
-#defaultbrowser chrome &&
 defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false &&
 sudo defaults write /Library/Preferences/com.apple.AppleMultitouchTrackpad Clicking -bool true && 
 sudo defaults write /Library/Preferences/.GlobalPreferences com.apple.mouse.tapBehavior -int 1 &&
@@ -49,12 +38,8 @@ defaults write com.apple.dock magnification -bool true &&
 defaults write com.apple.dock largesize -int 64 &&
 defaults write com.apple.dock persistent-apps -array &&
 dockutil --remove Downloads &&
-#dockutil --add "/Applications/Google Chrome.app" &&
-#dockutil --add "/Applications/Brave Browser.app" &&
-dockutil --add /Applications/Utilities/Terminal.app &&
+dockutil --add /Applications/Utilities/WezTerm.app &&
 dockutil --add /Applications/Visual\ Studio\ Code.app &&
-#dockutil --add "/Applications/Slack.app" &&
-#dockutil --add "/Applications/Studio 3T.app" &&
 osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true' &&
 defaults write -g com.apple.swipescrolldirection -bool false &&
 #cleanup
