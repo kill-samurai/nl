2b5c920e90335f260a5ac0172a75ff874e2b1a44
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2026-09-09 19:09:01 -0400

    added a new alias to aliases.fish to fetch data from the remote oracle server

--- dotfiles/fish/.config/fish/conf.d/aliases.fish
+++ dotfiles/fish/.config/fish/conf.d/aliases.fish
@@ -5,3 +5,11 @@ end
 function pip
     command pip3 $argv
 end
+
+function caribe --description 'Refresh Caribe data and restore the original Tailscale state'
+    if not command -q python3
+        echo 'Python 3 is required to refresh Caribe data.' >&2
+        return 1
+    end
+    python3 "$HOME/Documents/caribe/refresh_data.py" $argv
+end
